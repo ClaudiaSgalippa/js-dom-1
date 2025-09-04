431f2f7b175a5900d6887f6af06b0e9ff88ad037
ClaudiaSgalippa <claudia.sgappy@gmail.com>
2025-09-04 17:37:18 +0200

    Completamento esercizio

--- main.js
+++ main.js
@@ -7,12 +7,15 @@ console.log(accesa);
 let button = document.getElementById("bottone"); //Selezione bottone
 console.log(bottone);
 
-
+let spegniamoLaLampadina = true; //Impostazione valore booleano 
 
 //Funzione bottone
 
 button.addEventListener("click", //Impostazione evento "al premere il bottone"
     function onOff () {
-
+        if (spegniamoLaLampadina) {
+            turnOff.style.display = "none";
+            turnOn.style.display = "block";
+        }
     }
 )
