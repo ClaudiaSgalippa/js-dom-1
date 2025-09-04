e7ec56b8669ff594453c7a1da99297f8e59e824d
ClaudiaSgalippa <claudia.sgappy@gmail.com>
2025-09-04 17:22:51 +0200

    Piccole modifiche

--- main.js
+++ main.js
@@ -4,7 +4,15 @@ console.log(spenta);
 const turnOn = document.querySelector("#accesa"); //Selezione lampadina accesa
 console.log(accesa);
 
-const button = document.getElementById("bottone"); //Selezione bottone
+let button = document.getElementById("bottone"); //Selezione bottone
 console.log(bottone);
 
 
+
+//Funzione bottone
+
+button.addEventListener("click", //Impostazione evento "al premere il bottone"
+    function onOff () {
+
+    }
+)
