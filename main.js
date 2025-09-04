de6d5e92d6be6e553bea94150b6f6587da97238e
ClaudiaSgalippa <claudia.sgappy@gmail.com>
2025-09-04 16:46:11 +0200

    Modifiche e preparazione file JS

---
+++ main.js
@@ -0,0 +1,10 @@
+const turnOff = document.querySelector("#spenta"); //Selezione lampadina spenta
+console.log(spenta);
+
+const turnOn = document.querySelector("#accesa"); //Selezione lampadina accesa
+console.log(accesa);
+
+const button = document.getElementById("bottone"); //Selezione bottone
+console.log(bottone);
+
+
